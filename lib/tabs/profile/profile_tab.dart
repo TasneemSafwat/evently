7f41f5007676a3b6e07369b5011823463425e6ed
tasneem safwat <tasmarakby92@gmail.com>
2025-03-08 07:05:16 +0200

    implement profile header & logout button

--- lib/tabs/profile/profile_tab.dart
+++ lib/tabs/profile/profile_tab.dart
@@ -1,10 +1,49 @@
+import 'package:evently/app_theme.dart';
+import 'package:evently/tabs/profile/profile_header.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile'),
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Column(
+      children: [
+        ProfileHeader(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Spacer(),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 24),
+                    height: 56,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: AppTheme.red,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.logout_outlined,
+                          color: AppTheme.white,
+                        ),
+                        Text(
+                          'Logout',
+                          style: textTheme.titleLarge,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
